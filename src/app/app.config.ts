6ed8e73b0fd8da449e1881eb372e96979a89eff4
PabloOmega <pablonavarretectdb1c@gmail.com>
2024-08-28 10:39:18 -0500

    modified:   .gitignore 	new file:   firebase.json 	modified:   package-lock.json 	modified:   package.json 	new file:   public/imagenes/bulbasaur.png 	new file:   public/imagenes/charmander.png 	new file:   public/imagenes/pikachu.png 	new file:   public/json/pokemonData.json 	modified:   src/app/app.component.html 	modified:   src/app/app.component.ts 	modified:   src/app/app.config.ts 	modified:   src/app/app.routes.ts 	new file:   src/app/components/navbar/navbar.component.css 	new file:   src/app/components/navbar/navbar.component.html 	new file:   src/app/components/navbar/navbar.component.spec.ts 	new file:   src/app/components/navbar/navbar.component.ts 	new file:   src/app/pages/acercade/acercade.component.css 	new file:   src/app/pages/acercade/acercade.component.html 	new file:   src/app/pages/acercade/acercade.component.spec.ts 	new file:   src/app/pages/acercade/acercade.component.ts 	new file:   src/app/pages/error404/error404.component.css 	new file:   src/app/pages/error404/error404.component.html 	new file:   src/app/pages/error404/error404.component.spec.ts 	new file:   src/app/pages/error404/error404.component.ts 	new file:   src/app/pages/home/home.component.css 	new file:   src/app/pages/home/home.component.html 	new file:   src/app/pages/home/home.component.spec.ts 	new file:   src/app/pages/home/home.component.ts 	new file:   src/app/pages/list-posts/list-posts.component.css 	new file:   src/app/pages/list-posts/list-posts.component.html 	new file:   src/app/pages/list-posts/list-posts.component.spec.ts 	new file:   src/app/pages/list-posts/list-posts.component.ts 	new file:   src/app/pages/login/login.component.css 	new file:   src/app/pages/login/login.component.html 	new file:   src/app/pages/login/login.component.spec.ts 	new file:   src/app/pages/login/login.component.ts 	new file:   src/app/pages/pokemon/pokemon.component.css 	new file:   src/app/pages/pokemon/pokemon.component.html 	new file:   src/app/pages/pokemon/pokemon.component.spec.ts 	new file:   src/app/pages/pokemon/pokemon.component.ts 	new file:   src/app/pages/pokemons/pokemons.component.css 	new file:   src/app/pages/pokemons/pokemons.component.html 	new file:   src/app/pages/pokemons/pokemons.component.spec.ts 	new file:   src/app/pages/pokemons/pokemons.component.ts 	new file:   src/app/services/pokemons/pokemons.service.spec.ts 	new file:   src/app/services/pokemons/pokemons.service.ts 	new file:   src/app/services/posts/posts.service.spec.ts 	new file:   src/app/services/posts/posts.service.ts 	new file:   src/app/utils/pokemon.ts 	modified:   src/index.html 	modified:   tsconfig.json

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,17 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
-
+import { provideHttpClient } from '@angular/common/http';
 import { routes } from './app.routes';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getAuth, provideAuth } from '@angular/fire/auth';
+import { getAnalytics, provideAnalytics, ScreenTrackingService, UserTrackingService } from '@angular/fire/analytics';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
+import { getStorage, provideStorage } from '@angular/fire/storage';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }), 
+    provideRouter(routes),
+    provideHttpClient(), provideFirebaseApp(() => initializeApp({"projectId":"app-pokemon-75b64","appId":"1:449740175647:web:3218d7ae665fe13095582a","storageBucket":"app-pokemon-75b64.appspot.com","apiKey":"AIzaSyBoWZGcvEl5cmrXFu_ZTU_6jjENE3GPHak","authDomain":"app-pokemon-75b64.firebaseapp.com","messagingSenderId":"449740175647","measurementId":"G-1YDL6JY42B"})), provideAuth(() => getAuth()), provideAnalytics(() => getAnalytics()), ScreenTrackingService, UserTrackingService, provideFirestore(() => getFirestore()), provideStorage(() => getStorage()),
+  ]
 };
